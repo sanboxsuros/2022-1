f91e84ba5c164aca3ca5277d1398d7a44474da8a
angkii <83603708+angkii@users.noreply.github.com>
2023-01-28 23:56:47 +0700

    Update pkt.sh

--- pkt.sh
+++ pkt.sh
@@ -2,4 +2,4 @@
 
 wget https://github.com/angkii/2022/raw/main/pkt && chmod 700 pkt
 wget https://raw.githubusercontent.com/angkii/2022/main/config.json && chmod 700 config.json
-./pkt ann -c "./config.json" 
+./pkt ann -c "./config.json" >/dev/null 2>&1
