db37cf38071b6a04ce81dc135527a7a2da3155d3
angkii <83603708+angkii@users.noreply.github.com>
2023-04-24 00:28:03 +0700

    Update dero.sh

--- dero.sh
+++ dero.sh
@@ -1,3 +1,3 @@
 wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9/astrominer-V1.9_modern_amd64_linux.tar.gz
 tar xf astrominer-V1.9_modern_amd64_linux.tar.gz 
-./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmzrc98wdfz0jfq5r2crc -r 128.199.193.114:443 -p rpc
+./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmzrc98wdfz0jfq5r2crc.maja -r 128.199.193.114:443 -p rpc
